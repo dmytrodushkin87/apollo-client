2a3ef6c98b6b2a01c144b7d9202ddb5aed9e78c5
dmytrodushkin87 <dmytrodushkin87@gmail.com>
2021-11-04 07:06:23 +0700

    upload the project

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import ApolloBost, { gql } from 'apollo-boost';
+
+const client = new ApolloBost({
+  uri: 'http://localhost:4000'
+});
+
+const getUsers = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`
+
+client.query({
+  query: getUsers
+}).then((response) => {
+  let html = ''
+  response.data.users.forEach((user) => {
+    html += `
+      <div>
+        <h3>${user.name}</h3>
+      </div>
+    `
+  })
+  document.getElementById('users').innerHTML = html
+});
+
+const getPosts = gql`
+  query {
+    posts {
+      title
+      author {
+        name
+      }
+    }
+  }
+`
+client.query({
+  query: getPosts
+}).then((response) => {
+  let html = ''
+  response.data.posts.forEach((post) => {
+    html += `
+      <div>
+        <h3>${post.title}</h3>
+        <h4>${post.author.name}</h4>
+      </div>
+    `
+  })
+  document.getElementById('posts').innerHTML = html
+});
